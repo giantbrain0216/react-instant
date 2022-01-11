3bb0483b36d7c4afbf741a91252c8b60aa5546fd
valdim.expert@mail.ru <valdim.expert@mail.ru>
2022-01-11 23:35:57 +0900

    create Hit component
    This receives hit as a prop.

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,5 +25,22 @@ const Header = () => (
   </header>
 );
 
+const Hit = ({ hit }) => (
+  <a href={"/"}>
+    <div className="card">
+      <div className="card-image">
+        <img src={hit.image} alt={hit.name} className="image" />
+      </div>
+      <div className="card-contents">
+        <Highlight attribute="title" hit={hit} className="card-title" />
+        <Highlight attribute="year" hit={hit} className="card-year" />
+        <div className="card-rating">Rating: {hit.rating}</div>
+        <div className="card-genre">
+          <span>{hit.genre[0]}</span> <span>{hit.genre[1]}</span>
+        </div>
+      </div>
+    </div>
+  </a>
+);
 
 export default Search;
